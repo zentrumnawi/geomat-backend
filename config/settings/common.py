6600d79c81e61f9db4828f7b025bc21087ce49ab
christian <chgad.games@gmail.com>
2020-09-08 17:31:16 +0200

    Add correct media root.

--- config/settings/common.py
+++ config/settings/common.py
@@ -110,6 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+MEDIA_ROOT = ROOT_DIR("media/")
+MEDIA_URL = "media/"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
-    url(r"", include("solid_backend.urls"))
+    url(r"", include("solid_backend.urls")),
 ]
