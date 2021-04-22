34461ca2a97a1d3a78389a008e9494e8f6325d9d
christian <chgad.games@gmail.com>
2021-04-22 12:49:04 +0200

    Renmae minerals to name.

--- geomat_content/models.py
+++ geomat_content/models.py
@@ -34,7 +34,7 @@ class MineralType(BaseProfile):
         max_length=100, blank=True, verbose_name=_("trivial name")
     )
     variety = models.CharField(max_length=100, blank=True, verbose_name=_("variety"))
-    minerals = models.CharField(max_length=100, blank=True, verbose_name=_("minerals"))
+    name = models.CharField(max_length=100, blank=True, verbose_name=_("minerals"))
     mohs_scale = DecimalRangeField(null=True, blank=True)
     density = DecimalRangeField(null=True, blank=True)
     streak = models.CharField(max_length=100, verbose_name=_("streak"))
