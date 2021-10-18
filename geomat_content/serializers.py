109a0fb9a3ca5fcdd50cbe4409f9f9d9beb6e421
christian <chgad.games@gmail.com>
2021-10-18 15:57:28 +0200

    Make yasg docs work.

--- geomat_content/serializers.py
+++ geomat_content/serializers.py
@@ -4,13 +4,15 @@ from django.utils.translation import ugettext_lazy as _
 from solid_backend.photograph.serializers import PhotographSerializer
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field, extend_schema_serializer
+from drf_yasg import openapi
+
 
 from .models import MineralType, Property, Miscellaneous
 from drf_spectacular.utils import extend_schema_field
 from .models import CrystalSystem, MineralType
 
 
-class VerboseLabelField:
+class VerboseLabelField(serializers.Field):
 
     def bind(self, field_name, parent):
         super(VerboseLabelField, self).bind(field_name, parent)
@@ -46,21 +48,29 @@ class CrystalSystemField(serializers.CharField):
 
 
 @extend_schema_field({"type": "array", "items": {"type": "string"}})
-class ListVerboseField(VerboseLabelField, serializers.ListField):
+class ListVerboseField(VerboseLabelField):
 
     def __init__(self, choice_dict, **kwargs):
         super(ListVerboseField, self).__init__()
         self.choice_dict = dict(choice_dict)
 
-    def bind(self, field_name, parent):
-        super(ListVerboseField, self).bind(field_name, parent)
-        self.label = self.parent.Meta.model._meta.get_field(self.field_name).verbose_name
+    # def bind(self, field_name, parent):
+    #     super(ListVerboseField, self).bind(field_name, parent)
+    #     self.label = str(self.parent.Meta.model._meta.get_field(self.field_name).verbose_name)
 
     def to_representation(self, value):
         lst = []
         if value:
             lst = [self.choice_dict.get(choice) for choice in value]
         return lst
+
+    class Meta:
+        swagger_schema_fields = {
+            "type": openapi.TYPE_ARRAY,
+            "items" : {
+                "type": openapi.TYPE_STRING,
+            },
+        }
 
 
 class RangeOrSingleNumberField(VerboseLabelField, serializers.CharField):
