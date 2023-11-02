1a7b8ad85a958ae5fdcb4d91478295cc11b6f1df
Christian Grossmüller <chgad.games@gmail.com>
2023-11-02 14:10:46 +0100

    Create Stone model.

--- stone_content/models.py
+++ stone_content/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from solid_backend.content.models import SolidBaseProfile
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Stone(SolidBaseProfile):
+
+    class Meta:
+        verbose_name = _("Stein")
+        verbose_name_plural = _("Steine")
