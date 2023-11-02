536b7f34fc6767c1250ee8bcf2b9a0c001b8ce23
Christian Grossmüller <chgad.games@gmail.com>
2023-11-02 14:11:24 +0100

    Add Admin site for Stone model.

--- stone_content/admin.py
+++ stone_content/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Stone
 
+
+class StoneAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Stone
+
+
+admin.site.register(Stone, StoneAdmin)
 # Register your models here.
